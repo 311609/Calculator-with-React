6b4e97d79e637650182fe8ef6efb171877cadb7f
John Fredy Correa <johnfredycorrea75@gmail.com>
2024-04-14 18:43:19 -0600

    se sube proyecto a github

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import BotonClear from './components/BotonClear';
 import { useState } from 'react';
 import { evaluate } from 'mathjs';
 
-
 function App() {
-
   const [input, setInput] = useState('');
 
   const agregarInput = valor => {
@@ -18,18 +16,15 @@ function App() {
   const calcularResultado = () => {
     if (input) {
       setInput(evaluate(input));
-    }else {
-      alert('Ingresar valores para realizar calculo')
+    } else {
+      alert('Ingresar valores para realizar calculo');
     }
   };
 
   return (
     <div className='App'>
       <div className='adaschool-logo-contenedor'>
-        <img
-        src={adaSchoolLogo}
-        className='adaschool-logo'
-        alt='Logo de Ada School' />
+        <img src={adaSchoolLogo} className='adaschool-logo' alt='Logo de Ada School' />
       </div>
       <div className='contenedor-calculadora'>
         <Pantalla input={input} />
@@ -40,27 +35,25 @@ function App() {
           <Boton manejarClic={agregarInput}>+</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarClic={agregarInput}>4</Boton>
-        <Boton manejarClic={agregarInput}>5</Boton>
-        <Boton manejarClic={agregarInput}>6</Boton>
-        <Boton manejarClic={agregarInput}>-</Boton>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarClic={agregarInput}>7</Boton>
-        <Boton manejarClic={agregarInput}>8</Boton>
-        <Boton manejarClic={agregarInput}>9</Boton>
-        <Boton manejarClic={agregarInput}>*</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarClic={calcularResultado}>=</Boton>
-        <Boton manejarClic={agregarInput}>0</Boton>
-        <Boton manejarClic={agregarInput}>.</Boton>
-        <Boton manejarClic={agregarInput}>/</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
         </div>
         <div className='fila'>
-        <BotonClear manejarClear={() => setInput('')}>
-          Clear
-        </BotonClear>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
         </div>
       </div>
     </div>
--- src/components/Pantalla.jsx
+++ src/components/Pantalla.jsx
@@ -2,6 +2,7 @@
 import '../hojas-de-estilo/Pantalla.css';
 
 
+// eslint-disable-next-line react/prop-types
 const Pantalla = ( { input } ) => (
     <div className='input'>
     {input}
